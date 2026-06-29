d9f048ec324827c126fbea59afb39a17b81bb3c8
Aaron Chavez <aaronchavez.dev@gmail.com>
2026-06-29 23:30:19 +0800

    Repeating module D

--- Module_D/06_module_d/src/App.jsx
+++ Module_D/06_module_d/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 
-  function generateCaption(filename)
-  {
-    const base = filename.replace(/\.[^/.]+$/, "") //remove extension
-    return base 
-      .replace(/[-_]+/g, " ") // replaces underscore/dashes
-      .split(/\s+/)
-      .filter(Boolean)
-      .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
-      .join(" ")
-  }
-
+function generateCaption(filename)
+{
+  const base = filename.replace(/\.[^/.]+$/,"")
+  return base
+    .split(/\s+/)
+    .filter(Boolean)
+    .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ")
+}
 
 function App() {
   
@@ -18,13 +16,19 @@ function App() {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
 
 
+  function showSlide(nextIndex)
+  {
+    if(slides.length ===  0) return
 
+    const validIndex = (nextIndex + slides.length) % slides.length
+    setCurrentSlideIndex(validIndex)
+  }
 
   function handleFileInput(e)
   {
     const files = [...e.target.files].filter(file => file.type.startsWith("image/"))
 
-    if(files.length === 0) return 
+    if(files.length === 0 ) return 
 
     setSlides(files.map(
       file => ({
@@ -36,65 +40,58 @@ function App() {
     setCurrentSlideIndex(0)
   }
 
-  function showSlide(nextIndex)
-  {
-    if(slides.length === 0) return 
+  const current = slides[currentSlideIndex] || []
 
-    const validIndex = (nextIndex + slides.length) % slides.length 
-    setCurrentSlideIndex(validIndex)
-  }
-
-  const current = slides[currentSlideIndex]
- 
   return (
-   <div id="main-page">
-    <div id="slide-show">
-      {
-        slides.length === 0 
-          ? <p>No photos loaded</p>
-          : 
-          <figure
-            className="slide-frame slide-frame-incoming"
-          >
-            <img 
-              src={current.src}
-              alt={generateCaption(current.filename)}
-            />
-            <figcaption>{generateCaption(current.filename)}</figcaption>
-          </figure>
-      }
-    </div>
-    <aside>
-
-      <section id="drop-zone">
-        <label htmlFor="drop-zone">Drop Zone </label>
-        <input 
-          type="file"
-          name="drop-zone"
-          id="drop-zone"
-          multiple
-          accept="image/*"
-          onChange={handleFileInput}
-        
-        />
-      </section>
-
-      <section id="thumbnails">
-          {slides.map((slide,index) => {
-            <img 
-              key={slide.filename + index}
-              src={slide.src}
-              alt={generateCaption(slide.filename)}
-              className={index === currentSlideIndex ? "active" : ""}
-              onClick={() => showSlide(index)}
-            />
-          })
-
+    <div id="main-page">
+      <div id="slide-show">
+          {
+            slides.length === 0 
+              ? <p>No Photos Loaded</p>
+              :
+                <figure
+                  className='slide-frame slide-frame-incoming'
+                >
+                  <img 
+                    src={current.src}
+                    alt={generateCaption(current.filename)}
+                  />
+                  <figcaption>{generateCaption(current.filename)}</figcaption>
+                </figure>
           }
-      </section>
-    </aside>
-   </div>
+
+      </div>
+      <aside>
+        <section id="drop-zone">
+          <input 
+            type="file"
+            name="drop-zone"
+            id='drop-zone'
+            accept='image/*'
+            multiple
+            onChange={handleFileInput}
+          
+          />
+        </section>
+
+        <section id='thumbnails'>
+          {
+            slides.map((slide,index) => (
+              <img 
+                key={slide.filename + index}
+                src={slide.src}
+                alt={generateCaption(slide.filename)}
+                className= {index === currentSlideIndex ? "active" : ""}
+                onClick={() => showSlide(index)}
+              />
+            ))
+          }
+        </section>
+      </aside>
+    </div>
   )
+
+  
 }
 
 export default App
