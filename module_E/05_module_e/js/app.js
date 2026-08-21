85ee75d9a047e0f7848b53a8fd9c4b77381c0764
Aaron <aaronchavez.dev@gmail.com>
2026-08-21 16:14:26 +0800

    Review Module E

--- module_E/05_module_e/js/app.js
+++ module_E/05_module_e/js/app.js
@@ -1,38 +1,169 @@
 // markers 
+// const markers = document.querySelectorAll(".marker");
+// const attractionCards = document.querySelectorAll(".attractionCard");
+
+// markers.forEach((marker,index) => {
+//     const card = attractionCards[index];
+//     if(!card) return 
+
+//     marker.addEventListener("mouseover", () => card.classList.add("focus"));
+//     marker.addEventListener("mouseout", () => card.classList.remove("focus"));
+// })
+
 const markers = document.querySelectorAll(".marker");
 const attractionCards = document.querySelectorAll(".attractionCard");
 
 markers.forEach((marker,index) => {
     const card = attractionCards[index];
-    if(!card) return 
+    if(!card) return; 
 
     marker.addEventListener("mouseover", () => card.classList.add("focus"));
     marker.addEventListener("mouseout", () => card.classList.remove("focus"));
-})
+});
+
 
 // readLoud 
+// document.getElementById("readLoud")?.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const speech = new SpeechSynthesisUtterance(
+//         "Contact: 04 72 10 30 30 Address: Mairie de Lyon, 69205 Lyon cedex 01"
+//     );
+//     speech.lang = "fr-FR";
+//     window.speechSynthesis.speak(speech);
+// })
+
 document.getElementById("readLoud")?.addEventListener("click", (e) => {
     e.preventDefault();
+
     const speech = new SpeechSynthesisUtterance(
         "Contact: 04 72 10 30 30 Address: Mairie de Lyon, 69205 Lyon cedex 01"
     );
     speech.lang = "fr-FR";
     window.speechSynthesis.speak(speech);
+
 })
 
 // video 
+// const video = document.querySelector("video");
+// if(video)
+// {
+//     const observer = new IntersectionObserver(
+//         ([entry]) => (entry.isIntersecting ? entry.target.play() : entry.target.pause()),
+//         {threshold: 0.5}
+//     );
+
+//      observer.observe(video);
+
+//      document.addEventListener("visibilitychange", () => {
+//         if(document.visibilityState === "hidden") video.pause();
+//         else video.play();
+//      });
+// }
+
 const video = document.querySelector("video");
 if(video)
 {
     const observer = new IntersectionObserver(
-        ([entry]) => (entry.isIntersecting ? entry.target.play() : entry.target.pause()),
+        ([entry]) => (entry.isIntersecting ? entry.target.play() : entry.target.pause()), 
         {threshold: 0.5}
-    );
+    )
 
-     observer.observe(video);
+    observer.observe(video);
 
-     document.addEventListener("visibilitychange", () => {
-        if(document.visibilityState === "hidden") video.pause();
-        else video.play();
-     });
+    document.addEventListener("visibilityChange", () => {
+        if(document.visibilityState === "hidden") video.play();
+        else video.pause();
+    })
 }
+
+function couponGame()
+{
+    const result = document.getElementById("result");
+    const drawBtn = document.getElementById("drawBtn");
+    const restart = document.getElementById("restart");
+
+    const prizes = ["No Prize", "10% Discount Coupon", "100% Discount Coupon!!"];
+
+    drawBtn.onclick = () => {
+        const random = Math.floor(Math.random() *  prizes.length);
+        result.textContent = prizes[random];
+        result.classList.add("active");
+
+        drawBtn.disabled = true; 
+        restart.disabled = false; 
+    }
+
+    restart.onclick = () => {
+        result.textContent = "";
+        result.classList.remove("active");
+
+        drawBtn.disabled = false; 
+        restart.disabled = true;
+
+    }
+
+}
+
+
+async function loadReviews()
+{
+    const slides = document.getElementById("reviewSlides");
+    const res = await fetch("review.json");
+    const {reviews} = await res.json();
+
+    slides.innerHTML = reviews.map(({author, content, rating}) => `
+        <div class="reviewCard">
+            <p>${"⭐".repeat(rating)}<p>
+            <p>${content}</p>
+            <p>${author}<p>
+        </div>
+    `).join(" ");
+}
+
+
+// Review
+let currentReview = 0;
+
+function updateSlider()
+{
+    const cards = document.querySelectorAll("#reviewSlides .reviewCard");
+
+    cards.forEach((card, index) => {
+        card.className = "reviewCard " + (
+            index === currentReview ? "center" : 
+            index === currentReview - 1 ? "left" : 
+            index === currentReview + 1 ? "right" :
+            "hidden"
+
+        );
+    });
+
+    document.getElementById("prev").disabled = currentReview === 0;
+    document.getElementById("next").disabled = currentReview === cards.length - 1;
+
+}
+
+document.getElementById("prev").addEventListener("click", () => {
+    if(currentReview > 0)
+    {
+        currentReview--;
+        updateSlider();
+    }
+});
+
+
+document.getElementById("next").addEventListener("click", () => {
+   
+    const total = document.querySelectorAll("#reviewSlides .reviewCard").length;
+
+    if(currentReview < total - 1)
+    {
+        currentReview++;
+        updateSlider()
+    }
+});
+
+
+
+loadReviews();
+couponGame(); 
